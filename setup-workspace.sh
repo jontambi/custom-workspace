c4121dcea2e506d1c5ba42fd361528daa37b634f
John Tambi <jon.tambi@gmail.com>
2021-02-02 18:29:10 -0500

    ref

--- setup-workspace.sh
+++ setup-workspace.sh
@@ -27,7 +27,6 @@ ansible-playbook --extra-vars "ansible_sudo_pass=${sudoPass}" config-vim.yaml
 # Set URL latest version Terraform
 # For exclude alpha version add  egrep -v 'rc|beta|alpha'
 LATEST_TERRAFORM=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].builds[].url' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | egrep -v 'rc|beta|alpha' | egrep 'linux.*amd64' |tail -1)
-#ZIP_TERRAFORM=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].builds[].url' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | egrep -v 'rc|beta|alpha' | egrep 'linux.*amd64' |tail -1 |awk -F "/" '{print $NF}')
 
 
 # Set URL latest version Packer
@@ -35,4 +34,5 @@ LATEST_PACKER=$(curl -sL https://releases.hashicorp.com/packer/index.json | jq -
 
 echo "Install IaC Tools"
 ansible-playbook --extra-vars "ansible_sudo_pass=${sudoPass} latest_terraform=${LATEST_TERRAFORM} latest_packer=${LATEST_PACKER}" iac-tools.yaml
-#ansible-playbook --extra-vars "ansible_sudo_pass=${sudoPass} latest_terraform=${LATEST_TERRAFORM} zip_terraform=${ZIP_TERRAFORM} latest_packer=${LATEST_PACKER}" iac-tools.yaml
+
+#https://github.com/microsoft/terminal/issues/5625
